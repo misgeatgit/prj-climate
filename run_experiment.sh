a835831999a2949bfc950889c6c400767e176297
Misgana Bayetta <misgana.bayetta@gmail.com>
2020-01-30 20:36:40 +0800

    Incorporate voting

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,4 +3,8 @@ echo "Preparing Binarized data for MOSES"
 python generate_MOSES_training_data.py
 echo "Running Moses"
 sh run_anal.sh
+echo "Merging Predictor model outputs on test"
+python merge_predictors.py
 echo "Done"
+echo "MOSES approximate Floating value prediction via voting"
+#sh vote.sh
