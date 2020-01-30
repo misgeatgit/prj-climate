9560ea4646f43201ec38f437cc39f68ddee76d19
Misgana Bayetta <misgana.bayetta@gmail.com>
2020-01-30 20:34:48 +0800

    Dynamically calculate training and testing data size

--- run_anal.sh
+++ run_anal.sh
@@ -1,13 +1,34 @@
+
+# Create test(~20%) and train(~80%) data from the original
+GET_TRAIN_TEST_SIZE_PY=$(cat <<END
+# python code starts here
+import sys
+import pandas as pds
+
+file = sys.argv[1]
+#LA_years = int(file.split('LookAhead')[1].split('_Top')[0])
+df = pds.read_csv(file)
+size = df.shape[0]
+test_size = int(size*0.2)
+train_size = size - test_size
+print('{},{}'.format(train_size,test_size))
+#python code Ends here
+END
+)
+
 for exp_on_lookahead in `ls exp`; do
 	for exp_on_target in `ls exp/$exp_on_lookahead`; do
 		DIR=exp/$exp_on_lookahead/$exp_on_target
 		for csv in `ls $DIR/*.csv`; do
+			echo 'Working with:'$csv
+			res="$(python -c "$GET_TRAIN_TEST_SIZE_PY" $csv)"
+			IFS=', ' read -r -a array <<< "$res"
+			echo "train_test size: ${array[0]},${array[1]}"
 			train_file=$DIR/exp_train.csv
 			test_file=$DIR/exp_test.csv
-			# Create test(~20%) and train(~80%) data from the original
+			head -n${array[0]} $csv > $train_file
 			head -n1 $csv > $test_file #add header
-			tail -n26 $csv >> $test_file #
-			head -n106 $csv > $train_file
+			tail -n${array[1]} $csv >> $test_file #
 			# Run MOSES on the training data
 			combof=exp/$exp_on_lookahead/$exp_on_target/combo.txt
 			echo 'Running MOSESE on '$csv
