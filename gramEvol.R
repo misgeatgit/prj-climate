ac7372f08a5396a6f657ab2640559e7f44efc78d
Misgana Bayetta Belachew <misgana.bayetta@gmail.com>
2020-10-18 15:59:35 +0800

    Minor update

--- gramEvol.R
+++ gramEvol.R
@@ -1,8 +1,9 @@
 library("gramEvol")
 library("Metrics")
 library("sets")
+library("testit")
 
-MAX_ITER <- 100
+MAX_ITER <- 10000
 MAX_LA <- 30
 
 df = read.csv("data/ffx/natural_data_ffx.csv")
@@ -40,6 +41,7 @@ for( LA in LookAhead) {
     #Ocean <- XTrain[, "Ocean"]
 
     Y <- tail(Output, -LA)
+    assert("Output length equals Input Length", nrow(Y) == nrow(X))
     YTrain <- head(Y, TrainLen)
     YTest <- tail(Y, TestLen)
     Temperature <- YTrain[, "Temperature"]
@@ -54,34 +56,47 @@ for( LA in LookAhead) {
                                         var2, var3),
                            func = grule(sin, cos, tan, log, sqrt, exp),
                            op = grule('+', '-', '*', '/', '^'),
-                           var1 = grule(WMGHG), var2 = grule(Ozone),
-                           var3 = grule(TropAerDir))
+                           var1 = grule(WMGHG,WMGHG^n,n,exp(WMGHG)),
+                           var2 = grule(Ozone,Ozone^n,n,exp(Ozone)),
+                           var3 = grule(TropAerDir,TropAerDir^n,n,exp(TropAerDir)),
+                           n = grule(1,2,3,4,5))
     group1_ruleDef <- list(expr = grule(op(expr, expr), func(expr), var1,
                                         var2, var3, var4),
                            func = grule(sin, cos, tan, log, sqrt),
                            op = grule('+', '-', '*', '/', '^'),
-                           var1 = grule(WMGHG), var2 = grule(Ozone),
-                           var3 = grule(Land_Use),var4 = grule(TropAerDir))
+                           var1 = grule(WMGHG,WMGHG^n,n,exp(WMGHG)),
+                           var2 = grule(Ozone,Ozone^n,n,exp(Ozone)),
+                           var3 = grule(TropAerDir,TropAerDir^n,n,exp(TropAerDir)),
+                           var4 = grule(Land_Use,Land_Use^n,n,exp(Land_Use)),
+                           n=grule(1,2,3,3,4,5))
     group2_ruleDef <- list(expr = grule(op(expr, expr), func(expr), var1,
                                         var2, var3, var4),
                            func = grule(sin, cos, tan, log, sqrt),
                            op = grule('+', '-', '*', '/', '^'),
-                           var1 = grule(WMGHG), var2 = grule(Ozone),
-                           var3 = grule(TropAerInd), var4 = grule(TropAerDir))
+                           var1 = grule(WMGHG,WMGHG^n,n,exp(WMGHG)),
+                           var2 = grule(Ozone,Ozone^n,n,exp(Ozone)),
+                           var3 = grule(TropAerDir,TropAerDir^n,n,exp(TropAerDir)),
+                           var4 = grule(TropAerInd,TropAerInd^n,n,exp(TropAerInd)),
+                           n=grule(1,2,3,3,4,5))
     allVar_ruleDef <- list(expr = grule(op(expr, expr), func(expr), var1,
                                         var2, var3, var4, var5, var6, var7,
                                         var8, var9),
                            func = grule(sin, cos, tan, log, sqrt),
                            op = grule('+', '-', '*', '/', '^'),
-                           var1 = grule(WMGHG), var2 = grule(Ozone),
-                           var3 = grule(Solar), var4 = grule(Land_Use),
-                           var5 = grule(SnowAlb_BC), var6 = grule(Orbital),
-                           var7 = grule(TropAerDir), var8 = grule(TropAerInd),
-                           var9 = grule(StratAer))
+                           var1 = grule(WMGHG,WMGHG^n,n,exp(WMGHG)),
+                           var2 = grule(Ozone,Ozone^n,n,exp(Ozone)),
+                           var3 = grule(TropAerDir,TropAerDir^n,n,exp(TropAerDir)),
+                           var4 = grule(TropAerInd,TropAerInd^n,n,exp(TropAerInd)),
+                           var5 = grule(Land_Use,Land_Use^n,n,exp(Land_Use)),
+                           var6 = grule(Orbital,Orbital^n,n,exp(Orbital)),
+                           var7 = grule(Solar,Solar^n,n,exp(Solar)),
+                           var8 = grule(SnowAlb_BC,SnowAlb_BC^n,n,exp(SnowAlb_BC)),
+                           var9 = grule(StratAer,StratAer^n,n,exp(StratAer)),
+                           n=grule(1,2,3,3,4,5))
     ruleDefs <- list(pair(ruleDef=group0_ruleDef,name="group_0"),
-             pair(ruleDef=group1_ruleDef,name="group_1"),
-             pair(ruleDef=group2_ruleDef, name="group_2"),
-             pair(ruleDef=allVar_ruleDef,name="group_all"))
+                     pair(ruleDef=group1_ruleDef,name="group_1"))
+                     #pair(ruleDef=group2_ruleDef, name="group_2"),
+                     #pair(ruleDef=allVar_ruleDef,name="group_all"))
     #ruleDefs <- list(pair(ruleDef=group0_ruleDef,name="all_var"))
 
     for(P in ruleDefs) {
@@ -101,10 +116,10 @@ for( LA in LookAhead) {
         predictedOnTrain <- eval(ge$best$expressions, XTrain)
         metrics <- c(sprintf("%s", ge$best$expressions),
                      round(mae(actualTrain,predictedOnTrain), 2),
-                     round(mae(actualTest,predictedOnTrain), 2))
+                     round(mae(actualTest,predictedOnTest), 2))
         png(file=sprintf("%s_LA_%s.png",P$name, LA))
         plot(actualTest, col= "red", type="p", pch=19, xlab="Month",
-             ylab="Temperature", 
+             ylab="Temperature",
              main=sprintf("Predict: %s year\nModel: %s\nMAE= %s",
                            LA, metrics[1],metrics[3]))
         points(predictedOnTest, col = "blue", type = "p")
